24c07fa70d1211708abc0793d634cbe22b855395
naeger <naeger.robert@gmail.com>
2024-12-09 17:34:59 +0100

    uzpdated stupid versions

--- src/main/kotlin/com/example/Sockets.kt
+++ src/main/kotlin/com/example/Sockets.kt
@@ -5,14 +5,15 @@ import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureSockets() {
     install(WebSockets) {
         contentConverter = KotlinxWebsocketSerializationConverter(Json)
-        pingPeriod = 15.seconds
-        timeout = 15.seconds
+//        pingPeriod = 15.seconds
+//        timeout = 15.seconds
         maxFrameSize = Long.MAX_VALUE
         masking = false
     }
@@ -31,3 +32,6 @@ fun Application.configureSockets() {
         }
     }
 }
+
+@Serializable
+data class GameField(val gameField: List<String>)
