2aa2da19cced42194b0a8d0aa55ca8ddf59b5b97
naeger <naeger.robert@gmail.com>
2024-12-09 17:07:28 +0100

    a new kotlin version is required, but it is a RC though....

--- src/main/kotlin/com/example/Sockets.kt
+++ src/main/kotlin/com/example/Sockets.kt
@@ -1,13 +1,16 @@
 package com.example
 
+import io.ktor.serialization.kotlinx.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureSockets() {
     install(WebSockets) {
+        contentConverter = KotlinxWebsocketSerializationConverter(Json)
         pingPeriod = 15.seconds
         timeout = 15.seconds
         maxFrameSize = Long.MAX_VALUE
--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,8 +1,15 @@
 package com.example
 
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.websocket.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
+import io.ktor.websocket.*
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -18,4 +25,43 @@ class ApplicationTest {
         }
     }
 
+    @Test
+    fun testSocket() = testApplication {
+        application {
+            module()
+        }
+        val client = HttpClient(CIO) {
+            install(WebSockets)
+        }
+        client.webSocket(
+            host = "localhost",
+            port = 8080,
+            path = "/tictactoe"
+        ) {
+            val message = "Hello, WebSocket!"
+            send(Frame.Text(message))
+            println("Sent: $message")
+            val gameField = listOf("", "", "", "", "", "", "", "", "")
+
+           // sendSerialized(gameFieldSer)
+
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val frameText = frame.readText()
+                if(frameText.contains("YOU SAID")) {
+                    println("Received: ${frame.readText()}")
+                    break
+                }
+            }
+
+            val byeMessage = "bye"
+            send(Frame.Text(byeMessage))
+            println("Sent: $byeMessage")
+
+        }
+        client.close()
+    }
 }
+
+@Serializable
+data class GameField(val gameField : List<String>)
