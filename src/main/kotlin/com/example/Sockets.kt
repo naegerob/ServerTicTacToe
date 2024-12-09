5ac406f8c8214ef97686d87d040d022a7f8a2d1a
naeger <naeger.robert@gmail.com>
2024-12-09 16:41:21 +0100

    path changed

--- src/main/kotlin/com/example/Sockets.kt
+++ src/main/kotlin/com/example/Sockets.kt
@@ -1,16 +1,10 @@
 package com.example
 
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
 import kotlin.time.Duration.Companion.seconds
-import org.slf4j.event.*
 
 fun Application.configureSockets() {
     install(WebSockets) {
@@ -20,10 +14,11 @@ fun Application.configureSockets() {
         masking = false
     }
     routing {
-        webSocket("/ws") { // websocketSession
+        webSocket("/tictactoe") { // websocketSession
             for (frame in incoming) {
                 if (frame is Frame.Text) {
                     val text = frame.readText()
+                    println("Server: $text")
                     outgoing.send(Frame.Text("YOU SAID: $text"))
                     if (text.equals("bye", ignoreCase = true)) {
                         close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
