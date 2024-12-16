373b78d192aca0853c86eedd8e0559bd4fb786ba
naegerob <naeger.robert@gmail.com>
2024-12-16 19:54:28 +0100

    deserializing works

--- src/main/kotlin/com/example/Sockets.kt
+++ src/main/kotlin/com/example/Sockets.kt
@@ -8,9 +8,11 @@ import io.ktor.websocket.*
 import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 import model.GameField
 import java.time.Duration
+import kotlin.random.Random
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
@@ -25,14 +27,20 @@ fun Application.configureSockets() {
     }
     routing {
         webSocket("/tictactoe") {
-
             println("onConnect")
             try {
                 for (frame in incoming) {
                     val text = (frame as Frame.Text).readText()
+                    val gameField = Json.decodeFromString<GameField>(text)
+                    val randomNumber = Random.nextInt(0, 9)
+                    val gameFieldList = gameField.gameField
+                    // TODO: think about something else than string shit
 
+                    println(gameField)
                     outgoing.send(Frame.Text(text))
                 }
+            } catch (e: SerializationException) {
+                println("Failed to deserialize: ${e.message}")
             } catch (e: ClosedReceiveChannelException) {
                 println("onClose ${closeReason.await()}")
             } catch (e: Throwable) {
